f761902a76c1512d0d558460cc24859ecbe7c177
Ruben Hernandez <hernandzruben9@gmail.com>
2024-05-20 00:16:39 -0400

    gg

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,12 +13,14 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.myapplication.databinding.ActivityMainBinding
 
 import com.example.myapplication.service.AudioPlayer
 import com.example.myapplication.service.ReproductorMusica
 import com.example.myapplication.ui.player.Player
-
+import jp.wasabeef.glide.transformations.BlurTransformation
 
 
 class MainActivity : AppCompatActivity() {
@@ -44,8 +46,9 @@ class MainActivity : AppCompatActivity() {
         // Encuentra el layout que contiene el botón
         val includedLayout: View = findViewById(R.id.include_playing_bar)
 
-        // Encuentra el botón dentro del layout incluido
-        val btnShowMenu: ImageButton = includedLayout.findViewById(R.id.btn_more)
+        val current_track_image: ImageView = includedLayout.findViewById(R.id.current_track_image)
+
+
 
         val btnAmpliar: ImageButton = includedLayout.findViewById(R.id.btnAmpliar)
 
@@ -56,9 +59,12 @@ class MainActivity : AppCompatActivity() {
         val imgTransp: ImageView = menuNav.findViewById(R.id.imageView8)
 
 
+        Glide.with(includedLayout)
+            .load(R.drawable.album_cover) // Reemplaza con tu drawable o uri
+            .apply(RequestOptions.bitmapTransform(BlurTransformation(25,10))) // Ajusta el radio y el muestreo
+            .into(current_track_image)
 
-
-        btnShowMenu.setOnClickListener {
+        btnAmpliar.setOnClickListener {
             // Cambiar la visibilidad del menú
             imgTransp.visibility = View.VISIBLE
             menuNav.visibility =   if (menuNav.visibility == View.VISIBLE) View.GONE else View.VISIBLE
@@ -68,29 +74,29 @@ class MainActivity : AppCompatActivity() {
 
 
         // Asumiendo que estás dentro de una Activity
-        btnAmpliar.setOnClickListener {
-
-            if (ReproductorMusica.obtenerCancionActual() != null) {
-                val intent = Intent(this, Player::class.java)
-                startActivity(intent)
-                overridePendingTransition(
-                    com.google.android.material.R.anim.abc_popup_enter,
-                    com.google.android.material.R.anim.abc_popup_exit
-                )
-            }
-            else{
-                // Mostrar un diálogo indicando que no hay música reproduciéndose
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("Sin reproducción")
-                builder.setMessage("Actualmente no se está reproduciendo ninguna canción.")
-                builder.setPositiveButton("OK") { dialog, which ->
-                    dialog.dismiss()
-                }
-                val dialog: AlertDialog = builder.create()
-                dialog.show()
-            }
-
-        }
+//        btnAmpliar.setOnClickListener {
+//
+//            if (ReproductorMusica.obtenerCancionActual() != null) {
+//                val intent = Intent(this, Player::class.java)
+//                startActivity(intent)
+//                overridePendingTransition(
+//                    com.google.android.material.R.anim.abc_popup_enter,
+//                    com.google.android.material.R.anim.abc_popup_exit
+//                )
+//            }
+//            else{
+//                // Mostrar un diálogo indicando que no hay música reproduciéndose
+//                val builder = AlertDialog.Builder(this)
+//                builder.setTitle("Sin reproducción")
+//                builder.setMessage("Actualmente no se está reproduciendo ninguna canción.")
+//                builder.setPositiveButton("OK") { dialog, which ->
+//                    dialog.dismiss()
+//                }
+//                val dialog: AlertDialog = builder.create()
+//                dialog.show()
+//            }
+//
+//        }
 
 
 
